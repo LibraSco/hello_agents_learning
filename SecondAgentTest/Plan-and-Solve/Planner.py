f7c32aa518fc1504b81d877b23b232f6d3151e79
Libra_star <17870469120@139.com>
2026-06-07 18:45:08 +0800

    Create Planner.py

---
+++ SecondAgentTest/Plan-and-Solve/Planner.py
@@ -0,0 +1,31 @@
+import Planner_prompt
+import ast
+class Planner:
+    def __init__(self,llm_client):
+        self.llm_client=llm_client
+    def plan(self,question:str)->list[str]:
+        """
+        根据用户问题生成一个行动计划
+        """
+        prompt=Planner_prompt.PLANNER_PROMPT_TEMPLATE.format(question=question) #format 方法会将模板中的 {question} 占位符替换为 question 变量实际传入的值。
+        #为了生成计划，我们构建一个简单的消息列表
+        messages=[{"role":"user","content":prompt}]
+        print("---正在生成计划---")
+        #使用流式输出来获取完整计划
+        response_text=self.llm_client.think(messages=messages) or ""
+        print(f"✅ 计划已生成:\n{response_text}")
+        #解析LLM输出的列表字符串
+        try:
+            #找到```python和```之间的内容
+            plan_str = response_text.split("```python")[1].split("```")[0].strip()
+            # 使用ast.literal_eval来安全地执行字符串，将其转换为Python列表
+            plan = ast.literal_eval(plan_str)
+            return plan if isinstance(plan, list) else []
+        except(ValueError,SyntaxError,IndexError) as e:
+             print(f"❌ 解析计划时出错: {e}")
+             print(f"原始响应: {response_text}")
+             return []
+        except Exception as e:
+             print(f"❌ 解析计划时发生未知错误: {e}")
+             return []
+    
